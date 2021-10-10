6bcdd27e2549ff39c4d10ae8f32ba38f9cd65914
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2021-10-11 01:55:01 +0530

    training stage complete

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 from os import read
 from yaml import parse
-from utils.common import read_config
+from src.utils.model import create_model
+from src.utils.common import read_config
 from src.utils.data_management import get_data
 import argparse
 
@@ -8,8 +9,25 @@ def training(config_path):
     config = read_config(config_path)
 
     validation_datasize = config["params"]["validation_datasize"]
-    
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
+
+    LOSS_FUNCTION = config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["merrics"]
+    NUM_CLASSES = config["params"]["no_classes"]
+
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCHS = config['params']['epochs']
+    VALIDATION = (X_valid, y_valid)
+
+    history = model.fit(X_train, 
+                        y_train,
+                        epochs = EPOCHS, 
+                        validation_data = VALIDATION
+                        )
+
+
 
 
 if __name__ == "__main__":
