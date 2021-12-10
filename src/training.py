685fdf670c875e31c862937c4f4ce0f9f85381b9
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2021-12-10 23:19:07 +0530

    callbacks implementation started

--- src/training.py
+++ src/training.py
@@ -1,8 +1,10 @@
+import os
 from os import read
 from yaml import parse
-from src.utils.model import create_model
+from src.utils.model import create_model, save_model, save_plot
 from src.utils.common import read_config
 from src.utils.data_management import get_data
+from src.utils.callbacks import get_callbacks
 import argparse
 
 def training(config_path):
@@ -21,21 +23,38 @@ def training(config_path):
     EPOCHS = config['params']['epochs']
     VALIDATION = (X_valid, y_valid)
 
-    history = model.fit(X_train, 
+    CALLBACK_LIST = get_callbacks(config, X_train)
+
+    history = model.fit(X_train,
                         y_train,
-                        epochs = EPOCHS, 
-                        validation_data = VALIDATION
+                        epochs = EPOCHS,
+                        validation_data = VALIDATION,
+                        callbacks = CALLBACK_LIST
                         )
 
+    artifacts_dir = config['artifacts']['artifacts_dir']
 
+    model_name = config['artifatcs']['model_name']
+    model_dir = config['artifacts']['model_dir']
+
+    plot_dir = config['artifacts']['plot_dir']
+    plot_name = config['artifacts']['plot_name']
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path,exist_ok=True )
+    save_model(model, model_name, model_dir_path)
+
+    plot_dir_path = os.path.join(artifacts_dir, plot_dir)
+    os.makedirs(plot_dir_path, exist_ok=True )
+    save_plot(history, plot_name, plot_dir_path)
 
 
 if __name__ == "__main__":
-
     args = argparse.ArgumentParser()
 
-    args.add_argument("--config", "-c", default="config.yaml")
+    args.add_argument("--config", "-c", default = "config.yaml")
 
     parsed_args = args.parse_args()
 
-    training(config_path=parsed_args.config)
+    print(parsed_args)
+    #training(config_path=parsed_args.config)
